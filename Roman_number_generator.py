e551b63feaf58659128b634b7d2ba41df703aa56
Rube <ruben882002@gmail.com>
2022-09-12 18:22:17 -0500

    git test

---
+++ Roman_number_generator.py
@@ -0,0 +1,22 @@
+from random import randint
+rand = randint(1, 10)
+if rand == 10:
+    print("X")
+elif rand == 9:
+    print("IX")
+elif rand == 8:
+    print("VIII")
+elif rand == 7:
+    print("VII")
+elif rand == 6:
+    print("VI")
+elif rand == 5:
+    print("V")
+elif rand == 4:
+    print("IV")
+elif rand == 3:
+    print("III")
+elif rand == 2:
+    print("II")
+elif rand == 1:
+    print("I")

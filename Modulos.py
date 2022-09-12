852528558404c07431b376fa29a1496a2fc8fed4
Rube <ruben882002@gmail.com>
2022-09-12 18:23:01 -0500

    git test

---
+++ Modulos.py
@@ -0,0 +1,3 @@
+import random #importa el modulo de random
+from random import randint #importa la funcion randonint del modulo randon
+from random import * #importa todas las funciones del modulo randomffñ

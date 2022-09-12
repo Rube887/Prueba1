3a6f85c7bf8387cafe2fe930cb6cf838065bd641
Rube <ruben882002@gmail.com>
2022-09-12 18:24:19 -0500

    hola1

--- primerprograma.py
+++ primerprograma.py
@@ -1,1 +1,8 @@
-hola
+variable1 = int(input("Ingresa un numero positivo."))
+variable2 = variable1
+suma = 0
+while variable1 > 0:
+    suma += variable1
+    variable1 -=1
+print("La variable ingresada es " + str(variable2) + ".")
+print("La suma es " + str(suma) + ".")
